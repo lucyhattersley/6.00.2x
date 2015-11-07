c7a0087986a57e0112ca54d9dae8ff6ee534e54e
lucyhattersley <lucy.a.hattersley@gmail.com>
2015-11-07 13:40:03 +0000

    Correct

--- week2/l4p3/lecture4_problem3.py
+++ week2/l4p3/lecture4_problem3.py
@@ -8,19 +8,13 @@ def stdDevOfLengths(L):
         return float('NaN')
     X = []
     for t in L:
-        X.append(len(t))
+        X.append(float(len(t)))
     
     mean = sum(X) / len(X)
     
-    result = long(0.0)
+    result = 0.0
     for t in X:
         result += (t - mean)**2
     
     result = result / len(L)
     return result ** 0.5
-        
-#L = ['apples', 'oranges', 'kiwis', 'pineapples']
-#L = ['h', 'mi', 'uios', 'qkpsjixo', 'bmhiklywgy', 'ltpxfmympwwuex', 'o']
-L = ['h', 'mi', 'uios', 'qkpsjixo', 'bmhiklywgy', 'ltpxfmympwwuex', 'o']
-print stdDevOfLengths(L)
-
