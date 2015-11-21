5afbf9707f8060f0447e2b484892376e8e723221
lucyhattersley <lucy.a.hattersley@gmail.com>
2015-11-21 10:02:06 +0000

    PSET4: WIP! Still adapting trials

--- week4/PSET4/ProblemSet4/ProblemSet4/ps4.py
+++ week4/PSET4/ProblemSet4/ProblemSet4/ps4.py
@@ -21,11 +21,66 @@ def simulationDelayedTreatment(numTrials):
 
     numTrials: number of simulation runs to execute (an integer)
     """
+
+    ## Parameters stipulated in PSET4
+    numViruses  = 100
+    maxPop = 1000
+    maxBirthProb = 0.1
+    clearProb = 0.05
+    resistances = {'guttagonol': False} 
+    mutProb = 0.005
+
+    def Trial(insertionPoint):
+    # creating a list of virus instances
+        viruses = []
+        for i in range(numViruses):
+            viruses.append(ResistantVirus(maxBirthProb, clearProb, resistances, mutProb))
+
+        # creating the patient
+        patient = TreatedPatient(viruses, maxPop)
+
+        virusPops = []
+        resPops = []
+        
+        # combining both loops into a single 450 step loop
+        for i in range(450):
+            if i == insertionPoint:
+                patient.addPrescription('guttagonol')
+            try:
+                virusPops.append(patient.update())
+            except:
+                pass
+            try:
+                resPops.append(patient.getResistPop(['guttagonol']))
+            except:
+                resPops.append(0.0)
+        return virusPops, resPops
     
-    # TODO
+    # Running the trials
+    # This is incorrect
+    insertionPoints = [300, 150, 75, 0]
+    for insertionPoint in insertionPoints:
+        virusTrials = []
+        resTrials = []
+        for i in range(numTrials):
+            virusAdd, resAdd = Trial(insertionPoint)
+            virusTrials.append(virusAdd)
+            print "Numtrials: " + str(numTrials)
+            print "Virus Trials: " + str(virusTrials)
+            resTrials.append(resAdd)
+        virusAvg = [float(sum(col))/len(col) for col in zip(*virusTrials)]
+        resAvg = [float(sum(col))/len(col) for col in zip(*resTrials)]
+    
+        # plotting graphs for each trial. This will need to change to histogram
+        pylab.plot(range(len(virusAvg)), virusAvg, label = "Virus Average")
+        pylab.plot(range(len(resAvg)), resAvg, label = "Resistant Virus Average")
+        pylab.title("Resistant Virus Simulation. Insertion point: " + str(insertionPoint))
+        pylab.xlabel("time step")
+        pylab.ylabel("# viruses")
+        pylab.legend()
+        pylab.show()
 
-
-
+simulationDelayedTreatment(2)
 
 
 
