0f049f6429ded99327affc7cbc7ffed0a4268a67
lucyhattersley <lucy.a.hattersley@gmail.com>
2015-11-25 09:40:26 +0000

    WIP: PSET4: Trying to hack PS3 code to return data on clearProb

--- week4/PSET4/ProblemSet4/ProblemSet4/ps4.py
+++ week4/PSET4/ProblemSet4/ProblemSet4/ps4.py
@@ -8,7 +8,7 @@ from ps3b import *
 #
 # PROBLEM 1
 #        
-def simulationDelayedTreatment(numTrials):
+def simulationDelayedTreatment(numTrials = 100):
     """
     Runs simulations and make histograms for problem 1.
 
@@ -22,15 +22,7 @@ def simulationDelayedTreatment(numTrials):
     numTrials: number of simulation runs to execute (an integer)
     """
 
-    ## Parameters stipulated in PSET4
-    numViruses  = 100
-    maxPop = 1000
-    maxBirthProb = 0.1
-    clearProb = 0.05
-    resistances = {'guttagonol': False} 
-    mutProb = 0.005
-
-    def Trial(insertionPoint):
+    def Trial(insertionPoint = 150, numViruses = 100, maxPop = 1000, maxBirthProb = 0.1, clearProb = 0.05, resistances = {'guttagonol': False}, mutProb = 0.005):
         # creating a list of virus instances
         viruses = []
         for i in range(numViruses):
@@ -39,49 +31,102 @@ def simulationDelayedTreatment(numTrials):
         # creating the patient
         patient = TreatedPatient(viruses, maxPop)
 
-        # Running 450 steps (adding drug at insertionPoint)
-        # Returns the final drug count
-        for i in range(450):
-            if i == insertionPoint:
-                patient.addPrescription('guttagonol')
+        # update patient for insertion point steps
+        for i in range(insertionPoint):
             try:
                 patient.update()
             except:
                 pass
-        return patient.getTotalPop()
-    
-    # Running the trials
-    insertionPoints = [300, 150, 75, 0]
-    for insertionPoint in insertionPoints:
-
-        populations = []
-
-        for trial in range(numTrials):
-            trialResults = []
-            for t in range(trial):
-                trialResults.append(Trial(insertionPoint))
-            if sum(trialResults) > 0:
-                populations.append(trialResults)
-            print populations
         
-        results = []
-        for result in populations:
-            results.append(sum(result) / len(result))
-            print "results" + str(results)
+        # add guttagonol
+        patient.addPrescription('guttagonol')
+        
+        # update patient for another 150 steps
+        for i in range(150):
+            try:
+                patient.update()
+            except:
+                pass
 
-        try:
-            pylab.hist(results)
-            pylab.title('Virus trial: insertion point:' + str(insertionPoint))
-            pylab.xlabel('Total Population Values')
-            pylab.ylabel('No of trials')
-            pylab.show()
-        except:
-            print "Something went wrong"
-            print "Populations: " +str(populations)
+        # return patient populations
+        if patient.getTotalPop() > 0:
+            return patient.getTotalPop()
+        else:
+            return 0
 
-simulationDelayedTreatment(10)
+    # insertionPoint Trial
+#    insertionPoints = [300, 150, 75, 0]
+#
+#    for insertionPoint in insertionPoints:
+#        trialResults = []
+#        for trial in range(numTrials):
+#            trialResults.append(Trial(insertionPoint=insertionPoint))
+#        # drawing the histogram
+#        pylab.hist(trialResults, bins=10)
+#        pylab.title('Virus trial:  Insertion point = ' + str(insertionPoint))
+#        pylab.xlabel('Total Population Values')
+#        pylab.ylabel('No of trials')
+#        pylab.savefig('/Users/Lucy/Desktop/trial' + str(insertionPoint) + '.pdf')
+#        pylab.close()
 
+# maxPops trial
+#     maxPops = [1000, 2000, 4000, 8000, 10000]
+#     for maxPop in maxPops:
+#         trialResults = []
+#         for trial in range(numTrials):
+#             trialResults.append(Trial(maxPop=maxPop))
+#         # drawing the histogram
+#         pylab.hist(trialResults, bins=10)
+#         pylab.title('Virus trial:  Max Pop = ' + str(maxPop))
+#         pylab.xlabel('Total Population Values')
+#         pylab.ylabel('No of trials')
+#         pylab.savefig('/Users/Lucy/Desktop/trial' + str(maxPop) + '.pdf')
+#         pylab.close()
+#                      
 
+    # maxBirthProb trial
+#     maxBirthProbs = [0.1, 0.5, 0.9]
+#     for maxBirthProb in maxBirthProbs:
+#         trialResults = []
+#         for trial in range(numTrials):
+#             trialResults.append(Trial(maxBirthProb=maxBirthProb))
+#         # drawing the histogram
+#         pylab.hist(trialResults, bins=10)
+#         pylab.title('Virus trial:  maxBirthProb = ' + str(maxBirthProb))
+#         pylab.xlabel('Total Population Values')
+#         pylab.ylabel('No of trials')
+#         pylab.savefig('/Users/Lucy/Desktop/trial' + str(maxBirthProb) + '.pdf')
+#         pylab.close()
+#                      
+
+    #  clearProb trial
+#     clearProbs = [0.05, 0.06, 0.07, 0.08, 0.09, 0.1]
+#     for clearProb in clearProbs:
+#         trialResults = []
+#         for trial in range(numTrials):
+#             trialResults.append(Trial(clearProb=clearProb))
+#         # drawing the histogram
+#         pylab.hist(trialResults, bins=10)
+#         pylab.title('Virus trial:  clearProb = ' + str(clearProb))
+#         pylab.xlabel('Total Population Values')
+#         pylab.ylabel('No of trials')
+#         pylab.savefig('/Users/Lucy/Desktop/trial' + str(clearProb) + '.pdf')
+#         pylab.close()
+    
+    # Guttagonal resistance = True
+    trialResults = []
+    for trial in range(numTrials):
+        trialResults.append(Trial(resistances = {'guttagonol': True}))
+    # drawing the histogram
+    pylab.hist(trialResults, bins=10)
+    pylab.title('Virus trial:  guttagonol: True')
+    pylab.xlabel('Total Population Values')
+    pylab.ylabel('No of trials')
+    pylab.savefig('/Users/Lucy/Desktop/trial_guttagonol_true.pdf')
+    pylab.close()
+                 
+        
+simulationDelayedTreatment(100)
 
 #
 # PROBLEM 2
